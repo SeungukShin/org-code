02f164fdccffbc61834bc19e35db7e229e02f288
Seunguk Shin <seunguk.shin@gmail.com>
2020-11-28 22:55:05 +0900

    Add 'set state' command

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,20 @@ export class Org implements vscode.Disposable {
 	private log: Log;
 	private decoration: Decoration;
 	private folding: Folding;
+	private level: number;
+	private todoState: string;
+	private doneState: string;
+	private state: string;
 
 	constructor(context: vscode.ExtensionContext) {
 		this.config = Config.getInstance();
 		this.log = Log.getInstance();
 		this.decoration = new Decoration(context);
 		this.folding = new Folding(context);
+		this.level = this.config.get('headLevel');
+		this.todoState = this.config.get('todoState');
+		this.doneState = this.config.get('doneState');
+		this.state = this.todoState + ' ' + this.doneState;
 
 		// Register Folding Range Provider
 		if (this.config.get('fold')) {
@@ -24,7 +32,7 @@ export class Org implements vscode.Disposable {
 		}
 
 		// Register Commands
-//		context.subscriptions.push(vscode.commands.registerCommand('emacs-code.find.file', () => this.findFile()));
+		context.subscriptions.push(vscode.commands.registerCommand('org-code.set.state', () => this.setState()));
 
 		// open new document
 		vscode.window.onDidChangeActiveTextEditor(editor => {
@@ -74,6 +82,49 @@ export class Org implements vscode.Disposable {
 
 	dispose(): void {
 		this.decoration.dispose();
+	}
+
+	async setState(): Promise<void> {
+		// get current line
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return Promise.resolve();
+		}
+		const position = editor.selection.active;
+		const line = editor.document.lineAt(position.line);
+
+		// check head
+		const headRegex = new RegExp('^[*]{1,' + this.level.toString() + '}\\s+', 'g');
+		const headMatch = headRegex.exec(line.text);
+		if (!headMatch) {
+			return Promise.resolve();
+		}
+
+		// select new state
+		const state = await vscode.window.showQuickPick(this.state.split(' '));
+		if (!state) {
+			return Promise.resolve();
+		}
+
+		// remove old state
+		const stateRegex = new RegExp('^[*]{1,' + this.level.toString() + '}\\s+(' + this.state.replace(/\s/g, '|') + ')', 'g');
+		const stateMatch = stateRegex.exec(line.text);
+		if (stateMatch) {
+			console.log(stateMatch[0]);
+			await editor.edit((editBuilder) => {
+				const l = line.lineNumber;
+				const c = stateMatch[0].length;
+				const len = stateMatch[0].split(/\s+/)[1].length;
+				editBuilder.delete(new vscode.Range(l, c - len - 1, l, c));
+			})
+		}
+
+		// add new state
+		await editor.edit((editBuilder) => {
+			const l = line.lineNumber;
+			const c = headMatch[0].trim().length;
+			editBuilder.insert(new vscode.Position(l, c), ' ' + state);
+		})
 	}
 }
 
